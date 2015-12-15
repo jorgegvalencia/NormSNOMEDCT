15acb4641043c8bc2321fc5c73f4fe76fab233a0
jorgegvalencia <jorgegvalencia@gmail.com>
2015-12-15 14:51:42 +0100

    Refactoring code 3
    Databases

--- src/db/DBManager.java
+++ src/db/DBManager.java
@@ -3,9 +3,15 @@ package db;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import db.reports.CFReportJDBCTemplate;
 import db.reports.ConceptFrecuencyReport;
@@ -67,5 +73,59 @@ public class DBManager {
 		EligibilityCriteriaJDBCTemplate ecJDBCTemplate = (EligibilityCriteriaJDBCTemplate) context
 				.getBean("ecJDBCTemplate");
 		ecJDBCTemplate.create(ec);
+	}
+
+	public List<String> getSCTID(String cui) {
+		MetathesaurusJDBCTemplate metathesaurus = (MetathesaurusJDBCTemplate) context
+				.getBean("metathesaurusJDBCTemplate");
+		return metathesaurus.getSCTID(cui);
+	}
+
+	public String getFSN(String sctid) {
+		SnomedJDBCTemplate snomed = (SnomedJDBCTemplate) context.getBean("snomedJDBCTemplate");
+		return snomed.getFSN(sctid);
+	}
+
+	private static class MetathesaurusJDBCTemplate {
+		@SuppressWarnings("unused")
+		private DataSource dataSource;
+		private JdbcTemplate jdbcTemplateObject;
+
+		public void setDataSource(DataSource dataSource) {
+			this.dataSource = dataSource;
+			jdbcTemplateObject = new JdbcTemplate(dataSource);
+		}
+
+		public List<String> getSCTID(String cui) {
+			try {
+				String sql = "SELECT SCUI FROM metathesaurus.mrconso WHERE CUI= ? AND ISPREF='Y' AND SAB='SNOMEDCT_US' GROUP BY SCUI";
+				return jdbcTemplateObject.queryForList(sql, new Object[] { cui }, String.class);
+			} catch (EmptyResultDataAccessException e) {
+				return new ArrayList<String>();
+			}
+		}
+	}
+
+	private static class SnomedJDBCTemplate {
+		@SuppressWarnings("unused")
+		private DataSource dataSource;
+		private JdbcTemplate jdbcTemplateObject;
+
+		public void setDataSource(DataSource dataSource) {
+			this.dataSource = dataSource;
+			jdbcTemplateObject = new JdbcTemplate(dataSource);
+		}
+
+		public String getFSN(String sctid) {
+			try {
+				String sql = "SELECT term FROM curr_concept_s, curr_description_s WHERE curr_concept_s.id = curr_description_s.conceptid "
+						+ "AND curr_concept_s.id= ? AND curr_description_s.term LIKE '%(%)'"
+						+ "AND curr_description_s.term NOT LIKE '%(qualifier value)'" + "AND curr_concept_s.active='1'"
+						+ "AND curr_description_s.active='1';";
+				return jdbcTemplateObject.queryForObject(sql, new Object[] { sctid }, String.class);
+			} catch (EmptyResultDataAccessException e) {
+				return null;
+			}
+		}
 	}
 }
